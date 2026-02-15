0944c0da6d00b9b38c963d69a59b496c7df7265e
alifmbs <146939780+alifmathuf@users.noreply.github.com>
2026-02-15 13:23:57 +0700

    Update result.js

--- js/result.js
+++ js/result.js
@@ -151,3 +151,17 @@ function renderChart(correct, total) {
     }
   });
 }
+function downloadPDF(){
+
+  const element = document.getElementById("resultSheet");
+
+  const opt = {
+    margin:       10,
+    filename:     'hasil-ujian.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+  };
+
+  html2pdf().set(opt).from(element).save();
+}
