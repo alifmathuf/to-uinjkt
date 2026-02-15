122884451b731456b846650fc2444e25302688b5
alifmbs <146939780+alifmathuf@users.noreply.github.com>
2026-02-15 14:10:50 +0700

    Update result.js

--- js/result.js
+++ js/result.js
@@ -95,33 +95,6 @@ function saveToFirebaseLeaderboard(score, correct, total) {
 
 function generateAvatar(){
 
-  const user = Auth.getUser();
-  if(!user || !user.nama) return;
-
-  const name = user.nama.trim();
-  const firstLetter = name.charAt(0).toUpperCase();
-
-  const avatar = document.getElementById("avatar");
-  if(!avatar) return;
-
-  avatar.innerText = firstLetter;
-
-  // generate warna konsisten dari nama
-  const colors = [
-    "#2563eb",
-    "#0ea5e9",
-    "#14b8a6",
-    "#8b5cf6",
-    "#f59e0b",
-    "#ef4444"
-  ];
-
-  const index = name.charCodeAt(0) % colors.length;
-
-  avatar.style.background = colors[index];
-}
-
-generateAvatar();
 /* ================= CHART ================= */
 
 let chartInstance;
