cf8204a25925daa3b1957017d04ce5c9aa14cfa7
alifmbs <146939780+alifmathuf@users.noreply.github.com>
2026-02-16 15:10:15 +0700

    Update review.js

--- js/review.js
+++ js/review.js
@@ -73,3 +73,18 @@ function tampilkanReview(soal, jawaban){
   });
 
 }
+
+      function exportCasePDF(){
+
+  const element = document.getElementById("caseAnswerBox");
+
+  const opt = {
+    margin: 10,
+    filename: 'jawaban-studi-kasus.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+  };
+
+  html2pdf().set(opt).from(element).save();
+}
