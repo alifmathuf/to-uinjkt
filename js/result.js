4b270338d26505a853f2ac95f6f16c15ed3560d3
alifmbs <146939780+alifmathuf@users.noreply.github.com>
2026-02-18 14:57:16 +0700

    Update result.js

--- js/result.js
+++ js/result.js
@@ -27,12 +27,7 @@ db.ref(`exams/${user.id}`)
 .limitToLast(1)
 .once("value")
 .then(snapshot => {
-/* ================= TAMPILKAN INFO EXAM ================= */
-const mapelEl = document.getElementById("examMapel");
-const paketEl = document.getElementById("examPaket");
 
-if(mapelEl) mapelEl.innerText = (examData.mapel || "-").toUpperCase();
-if(paketEl) paketEl.innerText = examData.paket || "-";
   if (!snapshot.exists()) {
     alert("Belum ada hasil ujian.");
     window.location.href = "dashboard.html";
